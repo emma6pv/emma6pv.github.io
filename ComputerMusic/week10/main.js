c765d67b31372707c5a18c831d919a534940c46e
Emma Anderson <emmala1@uci.edu>
2020-05-16 23:49:18 -0700

    cleanup

--- ComputerMusic/week10/main.js
+++ ComputerMusic/week10/main.js
@@ -1,16 +1,16 @@
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Advanced_techniques
+// Audio Context
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
+
+// Path for soundfiles
 const rawPath = 'https://raw.githubusercontent.com/emma6pv/emma6pv.github.io/master/ComputerMusic/week10/TR-808/'
 
-
-//var crash = new Drum(getPath("crash.wav"), [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-//var clap = new Drum(getPath("clap.wav"), [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0]);
-
-
+// Variables
 let buffer;
-let secondsPerBeat = 60.0 / 120; //tempo
+var tempo = 120;
+var secondsPerBeat = 60.0 / tempo;
+
+
 
 window.addEventListener("click", function(event) {
   var clap = new Drum("clap.wav", [1,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0], audioCtx);
@@ -24,6 +24,8 @@ window.addEventListener("click", function(event) {
   setInterval(function(){ playSequence(clap); }, secondsPerBeat*4000);
 });
 
+
+
 function playSequence(drum) {
   let seq = drum.getSequence();
   var  time = 10;
@@ -36,15 +38,3 @@ function playSequence(drum) {
     time += secondsPerBeat*1000/4;
   }
 }
-
-// function playSomething(){
-//   let seq = [1,1,1,1,0,1,1,0,1,1,1,1,0,1,1,0];
-//   var  time = 0.0;
-//
-//   for (let i = 0; i < 16; i++){
-//     if (seq[i] == 1) {
-//       setTimeout(function(){ playSample(audioCtx, buffer); }, time);
-//     }
-//     time += secondsPerBeat*1000/4;
-//   }
-// }
